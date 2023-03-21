95c6b1ab549c6700e350ee6f63215c0a38cc5920
samerhassan95 <arch.samerhassan@gmail.com>
2023-03-21 04:21:17 +0200

    pagination for (authors,categories,books)

--- src/app/component/books/books.component.ts
+++ src/app/component/books/books.component.ts
@@ -10,13 +10,18 @@ import { BehaviorSubject } from 'rxjs';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent {
-      
+
 
 
     books!: book[] ;
     authors!:any[];
     newbook!:book[];
-   
+    paginated!: any[];
+    currentPage!: number;
+    pageSize!: number;
+    totalPages!: number;
+    pages: number[] = [];
+    count: number = 0;
   // books.name.tolowercase()
 
      constructor(private _BooksService: BooksService,private _AuthorsService: AuthorsService)
@@ -25,19 +30,61 @@ export class BooksComponent {
       _BooksService.getAllbooks().subscribe (books => {
              this.books = books.slice(0,20);
              console.log(this.books);
-      })  
-      
-      
-       
+             this.calculatePages();
+             this.paginated = this.books.slice(this.count, this.pageSize);
+             console.log(this.books);
+      })
+      this.currentPage = 1;
+      this.pageSize = 2;
+      this.totalPages = 5;
+      this.pages = [];
+      this.paginated = [];
+
+
 
     }
 
+    calculatePages() {
+      this.totalPages = Math.ceil(this.books.length / this.pageSize);
 
-     
+      console.log(this.totalPages);
+      this.pages = [];
+      for (let i = 1; i <= this.totalPages; i++) {
+        this.pages.push(i);
+      }
+    }
 
-     
+    setPage(page: number) {
+      this.currentPage = page;
+      // this.paginated = this.books.slice(this.count,this.pageSize);
+      console.log(page)
+    }
+
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+      // console.log('next');
+
+      this.count += 2;
+      this.pageSize += 2;
+      this.paginated=this.books.slice(this.count,this.pageSize)
+    }
+
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+      console.log('prev');
+      this.count -= 2;
+      this.pageSize -= 2;
+      this.paginated = this.books.slice(this.count,this.pageSize);
+    }
+
+
+
    OnInit() {
-           
+
   }
 
 }
--- src/app/component/categories/categories.component.ts
+++ src/app/component/categories/categories.component.ts
@@ -9,16 +9,64 @@ import {CatagoryService} from '../../services/category/catagory.service';
 export class CategoriesComponent {
 
   catagories!: catagory[] ;
+  paginated!: any[];
+  currentPage!: number;
+  pageSize!: number;
+  totalPages!: number;
+  pages: number[] = [];
+  count: number = 0;
 
   constructor(private _CatagoryService: CatagoryService)
      {
       _CatagoryService.getAllcategories().subscribe (catagories => {
              this.catagories = catagories.slice(0,12);
-            
+             this.calculatePages();
+             this.paginated = this.catagories.slice(this.count, this.pageSize);
+             console.log(this.catagories);
+
       })
-         
+      this.currentPage = 1;
+      this.pageSize = 2;
+      this.totalPages = 5;
+      this.pages = [];
+      this.paginated = [];
      }
 
 
+     calculatePages() {
+      this.totalPages = Math.ceil(this.catagories.length / this.pageSize);
 
+      console.log(this.totalPages);
+      this.pages = [];
+      for (let i = 1; i <= this.totalPages; i++) {
+        this.pages.push(i);
+      }
+    }
+
+    setPage(page: number) {
+      this.currentPage = page;
+      // this.paginated = this.books.slice(this.count,this.pageSize);
+      console.log(page)
+    }
+
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+      // console.log('next');
+
+      this.count += 2;
+      this.pageSize += 2;
+      this.paginated=this.catagories.slice(this.count,this.pageSize)
+    }
+
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+      console.log('prev');
+      this.count -= 2;
+      this.pageSize -= 2;
+      this.paginated = this.catagories.slice(this.count,this.pageSize);
+    }
 }
